77f684c33af4f306b847ed348fea5ba8902c9cc8
Samuel <sg1976387@gmail.com>
2025-12-05 08:19:01 -0300

    BugFix: solved bug in consultations seeds

--- src/prisma/seeds/consultations.ts
+++ src/prisma/seeds/consultations.ts
@@ -1,15 +1,47 @@
 import { prisma } from '../client';
+import { type ConsultationStatus, type ConsultationTypes } from '../../routes/consultations/types';
 
 export async function seedConsultations() {
-  const consultationsData = [
-    { userId: 1, doctorId: 1, date: new Date('2024-07-01'), notes: 'Checkup de rotina' },
+  const consultationsData: {
+    userId: number;
+    doctorId: number;
+    date: Date;
+    endTime: Date;
+    notes: string;
+    status: ConsultationStatus;
+    type: ConsultationTypes;
+    location: string;
+  }[] = [
+    {
+      userId: 1,
+      doctorId: 1,
+      date: new Date('2024-07-01T09:00:00Z'),
+      endTime: new Date('2024-07-01T10:30:00Z'),
+      notes: 'Checkup de rotina',
+      status: 'scheduled',
+      type: 'routine',
+      location: 'Clínica A',
+    },
     {
       userId: 1,
       doctorId: 2,
-      date: new Date('2024-07-03'),
+      date: new Date('2024-07-03T09:00:00Z'),
+      endTime: new Date('2024-07-03T11:00:00Z'),
       notes: 'Consulta para dores de cabeça frequentes',
+      status: 'scheduled',
+      type: 'exam',
+      location: 'Clínica C',
     },
-    { userId: 2, doctorId: 1, date: new Date('2024-07-02'), notes: 'Consulta para erupção cutânea' },
+    {
+      userId: 2,
+      doctorId: 1,
+      date: new Date('2024-07-02T09:00:00Z'),
+      endTime: new Date('2024-07-02T11:00:00Z'),
+      notes: 'Consulta para erupção cutânea',
+      status: 'scheduled',
+      type: 'checkup', 
+      location: 'Clínica B',
+    },
   ];
 
   for (const consultation of consultationsData) {
