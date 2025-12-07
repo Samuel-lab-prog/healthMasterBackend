159fd63794cc686dc5ca5b0f92f95587d5d961d6
Samuel <sg1976387@gmail.com>
2025-12-07 16:43:35 -0300

    Refactor: improved spacing in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from '@elysiajs/cors';
 import { openapi, fromTypes } from '@elysiajs/openapi';
 import { rateLimit } from 'elysia-rate-limit';
 import { BunAdapter } from 'elysia/adapter/bun';
+
 import { handleError } from './utils/handleError';
 import { sanitize } from './utils/xssClean';
 import * as l from './utils/logger';
@@ -17,6 +18,7 @@ const PREFIX = '/api/v1';
 const INSTANCE_NAME = 'mainServerInstance';
 const HOST_NAME = 'localhost';
 const PORT = Number(process.env.PORT) || 3000;
+
 const OPEN_API_SETTINGS = {
   path: '/docs',
   documentation: {
@@ -29,38 +31,46 @@ const OPEN_API_SETTINGS = {
   references: fromTypes(),
 };
 
-export default new Elysia({
+const ELYSIA_SETTINGS = {
   adapter: BunAdapter,
   name: INSTANCE_NAME,
   prefix: PREFIX,
-  sanitize: (value) => sanitize(value),
+  sanitize: (value: string) => sanitize(value),
   serve: {
     hostname: HOST_NAME,
     port: PORT,
   },
-})
+};
+
+export default new Elysia(ELYSIA_SETTINGS)
   .state('reqInitiatedAt', 0)
   .state('reqId', '')
+
   .onError(({ error, set, code, store }) => handleError(set, error, code, store.reqId))
+
   .use(rateLimit())
   .onStart(() => {
     l.log.info(`🚀 Server started at http://${HOST_NAME}:${PORT}${PREFIX}`);
   })
+
   .onRequest((ctx) => {
     ctx.store.reqId = crypto.randomUUID();
     if (ctx.request.url.includes('/docs')) return;
     ctx.store.reqInitiatedAt = performance.now();
     l.logOnRequest(ctx.request, ctx.store.reqId);
   })
+
   .onAfterResponse((ctx) => {
     if (ctx.request.url.includes('/docs')) return;
     l.logOnAfterResponse(ctx, ctx.store.reqId, ctx.store.reqInitiatedAt, ctx.response);
   })
+
   .use(cors())
   .use(authRouter)
   .use(userRouter)
   .use(doctorRouter)
   .use(consultationRouter)
   .use(referralRouter)
+
   .use(openapi(OPEN_API_SETTINGS))
   .listen({ hostname: HOST_NAME, port: PORT });
